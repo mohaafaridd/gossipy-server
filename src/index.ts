aa13565d5628471dd6d64564da29e7f74d97143f
Mohammed Farid <dev.mohammedfarid@gmail.com>
2020-01-21 20:17:37 +0200

    identified the prisma schema type in the usable schema

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,18 @@ const resolvers = {
     users: (parent, args, { prisma }, info) => {
       return prisma.users(null, info)
     },
+
+    stations: (parent, args, { prisma }, info) => {
+      return prisma.stations(null, info)
+    },
+
+    topics: (parent, args, { prisma }, info) => {
+      return prisma.topics(null, info)
+    },
+
+    comments: (parent, args, { prisma }, info) => {
+      return prisma.comments(null, info)
+    },
   },
 }
 
