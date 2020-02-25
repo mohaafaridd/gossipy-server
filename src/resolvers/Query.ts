015606ed92173e3c3823025ea33a6a495e9e6bd7
Mohammed Farid <dev.mohammedfarid@gmail.com>
2020-02-25 14:09:07 +0200

    implemented simple sorting by hot algorithm (moving to reddits ASAP)

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -1,4 +1,10 @@
-import { Prisma, User, Station, Membership } from '../generated/prisma-client'
+import {
+  Prisma,
+  User,
+  Station,
+  Membership,
+  Vote,
+} from '../generated/prisma-client'
 import { DateRange, SortType } from '../constants'
 import getSortingDate from '../utils/getSortingDate'
 // import { sortHot } from '../utils/sortMethods'
@@ -28,40 +34,61 @@ export default {
     info
   ) => {
     const finalDate = getSortingDate(dateRange)
-    console.log('finalDate', finalDate)
 
-    const topics = await prisma.topics({
-      where: {
-        createdAt_gte: finalDate,
-      },
-    })
-
-    const votes = await prisma.votes({
-      where: {
-        topic: {
-          createdAt_gte: finalDate,
-        },
-      },
-    })
-
-    // const [topic] = topics
-    // const hotRatio = sortHot(votes, topic)
-    // console.log('hotRatio', hotRatio)
+    const getTopic = async (vote: Vote) => {
+      return prisma.vote({ id: vote.id }).topic()
+    }
 
     switch (sort) {
-      case 'HOT':
-        console.log('HOT')
-        return 1
+      case 'HOT': {
+        // Get all votes in last (DATE_RANGE)
+        const votes = await prisma.votes({
+          where: { createdAt_gte: finalDate },
+        })
+
+        const topics = await Promise.all(votes.map(vote => getTopic(vote)))
+
+        // {'21657asd8zxc': 2, '545as74da98sd': 3}
+        const reduced = topics.reduce((acc, current) => {
+          if (current.id in acc) {
+            acc[current.id]++
+          } else {
+            acc[current.id] = 1
+          }
+
+          return acc
+        }, {})
+
+        const sorted = topics.sort((a, b) =>
+          reduced[a.id] > reduced[b.id]
+            ? -1
+            : reduced[a.id] < reduced[b.id]
+            ? 1
+            : 0
+        )
+
+        // Convert Objects to strings (To ease the removal)
+        const unique = new Set(sorted.map(e => JSON.stringify(e)))
+
+        // Converting back to Objects
+        const result = Array.from(unique).map(e => JSON.parse(e))
+
+        return result
+      }
 
       case 'TOP':
         console.log('TOP')
         return 1
 
       default:
-        console.log('NEW')
-        return 1
+        const topics = await prisma.topics({
+          orderBy: 'createdAt_DESC',
+          where: {
+            createdAt_gte: finalDate,
+          },
+        })
+        return topics
     }
-    return topics
   },
 
   comments: (parent, args, { prisma }: { prisma: Prisma }, info) => {
